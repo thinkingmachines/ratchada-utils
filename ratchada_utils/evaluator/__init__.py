0b90b069e575de892db8e2d4ee70d77cccdd5855
Zoon <zoon_p@thinkingmachin.es>
2024-07-26 09:35:45 +0700

    FIX

--- ratchada_utils/evaluator/__init__.py
+++ ratchada_utils/evaluator/__init__.py
@@ -18,4 +18,4 @@ isd: similar to evaluate, but give more concise information on each prediction a
     isd: Insertion-Substitution-Deletetion
 """
 
-from .evaluate_utils import evaluate, isd
+from .evaluate_utils import evaluate, isd, simple_evaluation
