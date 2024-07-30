58d2042b06e0d7e47b75845adaa24eae765b0fcd
Zoon <zoon_p@thinkingmachin.es>
2024-07-30 17:46:47 +0700

    fix ?

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def parse_requirements(filename):
 
 setup(
     name="ratchada_utils",
-    version="2.2.1",
+    version="2.2.2",
     packages=find_packages(include=["ratchada_utils", "ratchada_utils.*"]),
     package_data={
         "ratchada_utils.processor": ["*.json", "*.py"],
